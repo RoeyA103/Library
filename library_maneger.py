5aa5080fd0ba7ec89ee6a82e3e3f04146ba66a4a
roey_averbuch <roey6922@gmail.com>
2025-11-10 16:43:30 +0200

    adding file maneger claas

--- library_maneger.py
+++ library_maneger.py
@@ -1,8 +1,42 @@
 from modules.book import Book
 from modules.user import User
 from modules.library import Library
+from io.file_maneger import *
 from json import dumps , loads ,load , dump
-from types import SimpleNamespace
-import os
+
+
+class Maneger:
+    def __init__(self):
+        self.library = Library()
+        self.library.books = Maneger.load_books()
+        self.library.users = Maneger.load_users()
+
+    @classmethod
+    def load_books():
+        books = read_books_from_file()
+        return [Book(**data) for data in books]
+
+    @classmethod
+    def load_users():
+        users = read_users_from_file()
+        return  [User(**data) for data in users]
+            
+    def creat_user(self)-> User:
+        user_name = input("enter user name:\n")
+        user = User(user_name)
+        return user
+    
+    def creat_book(self)-> Book:
+        book_title = input("enter book title:\n")
+        book_author = input("enter book  author")
+        return Book(book_author,book_title)
+    
+    def sign_user(self,user:User):
+        self.library.add_user(user)
+        write_user_to_file(user)
+
+    def sign_book(self,book:Book):
+        self.library.add_book(book)
+        write_book_to_file(book)
 
 
