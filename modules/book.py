a7f58b04f669e79079927b236cf848121862c465
meyir moshe <mma088651259@gmail.com>
2025-11-10 11:20:59 +0200

    added functions

--- modules/book.py
+++ modules/book.py
@@ -10,12 +10,12 @@ class Book:
     def __str__(self):
         return f" the name book is: {self.title} the author book is: {self.author} Serial number: {self.isbn}"
 
-    def set_available(self,flag:bool):
+    def set_available(self,flag:bool) -> None:
         if isinstance(flag,bool):
             self.__is_available = flag
         else:
             raise ValueError("can add only Boolyan object")
 
-    def is_available(self):
+    def is_available(self) -> bool:
         return self.__is_available
 
--- modules/library.py
+++ modules/library.py
@@ -9,5 +9,30 @@ class Library:
         books = ", ".join(book.title for book in self.books)
         users = ", ".join(user.name for user in self.users)
         return f"books: {books}\nusers: {users}"
-    
+
+    def add_book(self,book:Book)->None:
+        if isinstance(book,Book):
+            self.books.append(book)
+            print("The book has been successfully added")
+        else:
+            ValueError("did yue cen append only book of type Book")
+
+    def add_user(self,user:User)->None:
+        if isinstance(user,User):
+            self.users.append(user)
+            print("The user has been successfully added")
+        else:
+            ValueError("did yue cen append only user of type User")
+
+    def return_book(self,user_id,book_isbn)->None:
+        book = find_book(book_isbn)
+        user = find_user(user_id)
+        if book and user:
+            book.set_available(True)
+            user.remove_book(book)
+
+    def list_available_books(self) ->list:
+        list_available = [book for book in self.books if book.is_available()]
+        return list_available
+
     
