f606e01f9ba41a71bd8f5fbbea466584bd0ff259
roey_averbuch <roey6922@gmail.com>
2025-11-10 16:52:06 +0200

    fixing path in io

--- io/file_maneger.py
+++ io/file_maneger.py
@@ -4,22 +4,22 @@ from json import load , dump
 
 
 def write_book_to_file(book:Book)->None:
-    with open("test.json", "a") as file:
+    with open("books.json", "a") as file:
         dump(book.__dict__, file, indent=4)
 
 
 def read_books_from_file() ->list[Book]:
-    with open("test.json", "r") as file:
+    with open("books.json", "r") as file:
         books = load(file)
     return books
 
 
 def write_user_to_file(user:User)->None:
-    with open("test.json", "a") as file:
+    with open("database\users.json", "a") as file:
         dump(user.__dict__, file, indent=4)
 
 def read_users_from_file() ->list[User]:
-    with open("test.json", "r") as file:
+    with open("database\users.json", "r") as file:
         users = load(file)
     return users
 
