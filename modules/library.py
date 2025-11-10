71843f8eafebbbd3c6d9df14aa753de82ff4c9c3
roey_averbuch <roey6922@gmail.com>
2025-11-10 11:22:38 +0200

    new functions

--- modules/library.py
+++ modules/library.py
@@ -10,4 +10,27 @@ class Library:
         users = ", ".join(user.name for user in self.users)
         return f"books: {books}\nusers: {users}"
     
+    def find_user(self, user_id: int) -> User:
+        for user in self.users:
+            if user.id == user_id:
+                return user
+        return None
     
+    def find_book(self,book_id:int)-> Book:
+        for book in self.bookss:
+            if book.id == book_id:
+                return book
+        return None
+    
+    def borrow_book(self,user_id:int,book_id:int)-> None:
+        user = self.find_user(user_id)
+        book = self.find_book(book_id)
+        book.set_available(False)
+        user.add_book(book)
+
+    def search_book(self,book_outhor:str,book_title:str)-> bool:
+        for book in self.books:
+            if book.outher == book_outhor or book.title == book_title:
+                return True
+        return False
+   
