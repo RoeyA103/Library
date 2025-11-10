21666bf3c526e74235e380956593fff852d45a27
roey_averbuch <roey6922@gmail.com>
2025-11-10 10:06:44 +0200

    adding init file + add book func in user

--- modules/user.py
+++ modules/user.py
@@ -1,13 +1,18 @@
+from modules.book import Book
 class User:
     id = 100
     def __init__(self,name:str,id:int):
         self.name = name
         self.id = User.id
         User.id += 1
-        self.borrowed_books = []
+        self.__borrowed_books = []
 
     def __str__(self):
         return f"""name:{self.name}, id:{self.id},
-        borrowed books:{", ".join(book.title for book in self.borrowed_books)}"""
+        borrowed books:{", ".join(book.title for book in self.__borrowed_books)}"""
     
-    
+    def add_book(self,book)-> None:
+        if  isinstance(Book,book):
+            self.__borrowed_books.append(book)
+        else:
+            raise ValueError("can add only Book object")
