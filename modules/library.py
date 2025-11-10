7141162e08a3a6de57e06fd93c4e379879f47ee6
roey_averbuch <roey6922@gmail.com>
2025-11-10 10:25:02 +0200

    library

--- modules/library.py
+++ modules/library.py
@@ -1,6 +1,13 @@
+from modules.book import Book
+from modules.user import User
 class Library:
     def __init__(self):
-        pass
+        self.books = []
+        self.users = []
 
     def __str__(self):
-        pass
+        books = ", ".join(book.title for book in self.books)
+        users = ", ".join(user.name for user in self.users)
+        return f"books: {books}\nusers: {users}"
+    
+    
